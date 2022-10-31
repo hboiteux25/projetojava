899edf058fdfcbad85c241d8e819f814d6cdd9e0
Henrique Boiteux <92280899+hboiteux25@users.noreply.github.com>
2022-10-31 09:22:33 -0300

    Criação do projeto

---
+++ primeiroProgramaJava/src/cursojava/executavel/PrimeiroCalsseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiroCalsseJava {
+	
+	/*Main é um método auto executavel em JAVA*/
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Meu primeiro programa java");
+	}
+
+}
